9202bb6681aa36a324eda76d24acd4f9eacf8d1e
Vladosicc <belov.vladosicc@yandex.ru>
2021-03-22 14:17:11 +0300

    add lab1

--- script.js
+++ script.js
@@ -1,149 +1,49 @@
-const connect = "https://stankinlab-api.herokuapp.com/dis/";
+var before = 0;
 
 function start()
 {
-  getajax();
+  before = 0;
+  test1();
 }
 
-function getajax()
-{   
-  console.log("GET");
-  $.ajax({
-    type: "GET",
-    url: connect + document.getElementById('UserIdGET').value,
-    success: function(msg){
-      console.log(msg);
-      console.log(msg.data);
-      var i = 0;
-      document.getElementById('table').innerHTML = "";
-      while (true)
+function test1()
+{
+    document.getElementById("error").style.display = "none";
+    var request = new XMLHttpRequest();
+    var url = "https://api.giphy.com/v1/gifs/search?api_key=";
+    var token = "frT9CU8K1SO8FAInTXlIUtV6EfHGI9dA";
+    console.log(url + token + '&q=' + document.getElementById("q").value + '&limit=' + document.getElementById("range").value + '&offset=' + before + '&rating=g&lang=' + document.getElementById("language").value);
+    
+    request.open('GET', url + token + '&q=' + document.getElementById("q").value + '&limit=' + document.getElementById("range").value + '&offset=' + before + '&rating=g&lang=' + document.getElementById("language").value);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+      var ts = request.response;
+      if(ts.pagination.count === 0)
       {
-        try
-        {
-          var tr = document.createElement("tr");
-          var UserId = document.createElement("th");
-          UserId.innerHTML = msg.data[i].UserId;
-          tr.appendChild(UserId);
-          var td = document.createElement("td");
-          td.innerHTML = msg.data[i].NickName;
-          tr.appendChild(td);
-          td = document.createElement("td");
-          td.innerHTML = msg.data[i].GuildName;
-          tr.appendChild(td);
-          td = document.createElement("td");
-          td.innerHTML = msg.data[i].IsBot;
-          tr.appendChild(td);
-          td = document.createElement("td");
-          td.innerHTML = msg.data[i].AvatarUrl;
-          tr.appendChild(td);
-          document.getElementById('table').appendChild(tr);
-          i++;
-        }
-        catch
-        {
-          break;
-        }
+        document.getElementById("error").style.display = "inline";
       }
+      console.log(ts);
+      var images = document.getElementById("images");
+      images.innerHTML = "";
+      for(var i = 1; i <= document.getElementById("range").value; i++)
+      {
+          var img = document.createElement("img");
+          img.style.margin = "10px 0 0 55px";
+          img.src = ts.data[i-1].images.downsized.url;
+          images.appendChild(img);
+          //document.getElementById("img" + i).src=ts.data[i-1].images.downsized.url
+      }
+      document.getElementById("btnnext").style.display = "inline";
+      before += parseInt(document.getElementById("range").value);
+      console.log(before);
     }
-  });
 }
 
-function postajax()
+function rangeChange()
 {
-  console.log("POST");
-  if(document.getElementById('UserIdPOST').value == "")
-  {
-    document.getElementById('UserIdPOST').classList.add("is-invalid");
-    return;
-  }
-  else
-  {
-    document.getElementById('UserIdPOST').classList.remove("is-invalid");
-  }
-  var id = parseInt(document.getElementById('UserIdPOST').value);
-  $.ajax({
-    type: "POST",
-    url: connect,
-    
-    data: "UserId="+ id +"&NickName=" + document.getElementById('NickNamePOST').value + "&GuildName=" + document.getElementById('GuildNickNamePOST').value + "&IsBot="+ document.getElementById('IsBotPOST').checked + "&AvatarUrl=" + document.getElementById('AvatarUrlPOST').value,
-    success: function(msg){
-      console.log(msg);
-      getajax();
-    }
-  });
-}
-
-function putajax()
-{
-  console.log("PUT");
-  if(document.getElementById('UserIdPUT').value == "")
-  {
-    document.getElementById('UserIdPUT').classList.add("is-invalid");
-    return;
-  }
-  else
-  {
-    document.getElementById('UserIdPUT').classList.remove("is-invalid");
-  }
-  var id = parseInt(document.getElementById('UserIdPUT').value);
-  $.ajax({
-    type: "PUT",
-    url: connect + id,
-    
-    data: "UserId="+ id +"&NickName=" + document.getElementById('NickNamePUT').value + "&GuildName=" + document.getElementById('GuildNickNamePUT').value + "&IsBot="+ document.getElementById('IsBotPUT').checked + "&AvatarUrl=" + document.getElementById('AvatarUrlPUT').value,
-    success: function(msg){
-      console.log(msg);
-      getajax();
-    }
-  });
-}
-
-function deleteajax()
-{
-  console.log("DELETE");
-  $.ajax({
-    type: "DELETE",
-    url: connect + document.getElementById('UserIdDELETE').value,
-    success: function(msg){
-      console.log(msg);
-      getajax();
-    }
-  });
-}
-
-function methodChanged()
-{
-  switch(document.getElementById('methods').value)
-  {
-    case '1':
-      document.getElementById('formGet').style.display = "inline";
-      document.getElementById('formPost').style.display = "none";
-      document.getElementById('formPut').style.display = "none";
-      document.getElementById('formDelete').style.display = "none";
-      break;
-    case '2':
-      document.getElementById('formGet').style.display = "none";
-      document.getElementById('formPost').style.display = "inline";
-      document.getElementById('formPut').style.display = "none";
-      document.getElementById('formDelete').style.display = "none";
-      break;
-    case '3':
-      document.getElementById('formGet').style.display = "none";
-      document.getElementById('formPost').style.display = "none";
-      document.getElementById('formPut').style.display = "inline";
-      document.getElementById('formDelete').style.display = "none";
-      break;
-    case '4':
-      document.getElementById('formGet').style.display = "none";
-      document.getElementById('formPost').style.display = "none";
-      document.getElementById('formPut').style.display = "none";
-      document.getElementById('formDelete').style.display = "inline";
-      break;
-    default:
-      document.getElementById('formGet').style.display = "inline";
-      document.getElementById('formPost').style.display = "none";
-      document.getElementById('formPut').style.display = "none";
-      document.getElementById('formDelete').style.display = "none";
-      break;
-  }
+  console.log(document.getElementById("range").value);
+  document.getElementById("rangeCount").innerHTML=document.getElementById("range").value;
+  console.log(document.getElementById("rangeCount").innerHTML);
 }
